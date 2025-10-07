35bbdaf4c46892e9b2d1c0c9bfac2d5c468460e1
Amit Singh <singhamitchs19@gmail.com>
2025-10-08 00:48:13 +0530

    debug for calling cloudinary

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,11 +26,16 @@ const registerUser = asyncHandler( async (req, res) => {
         }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    
+
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
     }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = coverImageLocalPath
+    ? await uploadOnCloudinary(coverImageLocalPath)
+    : null;
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
